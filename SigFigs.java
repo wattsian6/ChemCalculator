095af594e8c4cf07e6f065b8270562db9218cec2
bac2001 <clonigerba@appstate.edu>
2017-02-28 18:48:28 -0500

    Update SigFigs.java
    Updated SigFigs to work on numbers that need no rounding, yet the truncating must still be done after the radix.

--- SigFigs.java
+++ SigFigs.java
@@ -9,44 +9,50 @@ public class SigFigs
 {
     public static void main (String[]args)
     {
-         toSigFigs(4.957, 3);
-      
+         toSigFigs(4.953, 3); //Takes double input and desired sigfigs
+       
     }
 
     public static void toSigFigs(double num, int figs)
     {
-        String nums = String.valueOf(num);
+        String nums = String.valueOf(num);      //cast double value to a String
         double number = num; 
-        int key = figs - 1;
-      
+        int key = figs - 1;                     //accounts for java's values starting at 0. Key is where the number will be "cut off"
+     
 
         if (nums.contains("."))
-        key +=1;
+        key +=1;                                //takes the radix into account
        
-   
 
-        if (nums.charAt(key) > 4 && nums.charAt(key) !='9')
+
+        if (nums.charAt(key+ 1) > 52 && nums.charAt(key) !='9')         //shows that rounding is needed
         {
-            int old = Character.getNumericValue(nums.charAt(key));
-            int newer = old + 1;
-            char digit = (char)(newer + '0');
-
-     
-            
-            StringBuilder  finite = new StringBuilder(nums);
+            int old = Character.getNumericValue(nums.charAt(key));      //grab value needing to be rounded
+            int newer = old + 1;                                        //increment it by one
+            char digit = (char)(newer + '0');                           //cast it to a char
+      
+            StringBuilder  finite = new StringBuilder(nums);            //rebuild our string with the new rounded digit
             finite.setCharAt(key, digit);
 
-            String finalFinite = finite.substring(0, Math.min(finite.length(), key + 1));
+            String finalFinite = finite.substring(0, Math.min(finite.length(), key + 1));   
     
-            System.out.println(finalFinite);
+            System.out.println(finalFinite);        //format our string to have a max # of chars (sig figs) and print
+    
 
-    
+        }
+
+          
+        else if (nums.charAt(key+1) < 52 )              //shows no rounding needed
+        {
+           String finalFinite = nums.substring(0, Math.min(nums.length(), key + 1));    
+           System.out.println(finalFinite);        //format our string to have a max # of chars (sig figs) and print
         }
 
         else
         {
-            System.out.println("Currently Invalid Operation");
-        }
-    }
 
+        }
+
+    }
 }
+
