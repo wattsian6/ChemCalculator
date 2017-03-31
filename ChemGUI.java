cea7dad835dc003cf5c21ae8630107bb9ee1a892
bac2001 <clonigerba@appstate.edu>
2017-03-31 17:07:14 -0400

    Update ChemGUI.java

--- ChemGUI.java
+++ ChemGUI.java
@@ -20,8 +20,22 @@ import javax.swing.JComboBox;
  */
 public class Calculator2
 {
-	private JComboBox dropDown;
-    private JTextField field;
+    private JComboBox dropDown;
+	
+    private JLabel eqLabel;
+    private JLabel var1;
+    private JLabel var2;
+    private JLabel var3;
+    private JLabel var4;
+    private JLabel un1;
+    private JLabel un2;
+    private JLabel un3;    
+    private JLabel un4;
+
+    private JTextField field1;
+    private JTextField field2;
+    private JTextField field3;
+    private JTextField field4;
     private JLabel resultLabel;
     private JButton calculateButton;
     private JButton clearButton;
@@ -46,6 +60,7 @@ public class Calculator2
     
         
         helper();
+	panel2 = new JPanel();
         helper2();
         calcFrame.pack();
         calcFrame.setVisible(true);
@@ -63,10 +78,13 @@ public class Calculator2
 
 		dropDown = new JComboBox(equations);
         JPanel panel = new JPanel();
+	eqLabel = new JLabel("EQATION");
+	eqLabel.setName("eqLabel");
+	panel.add(eqLabel);
         resultLabel = new JLabel("Result = ");
         resultLabel.setName("resultLabel");
         panel.add(resultLabel);
-        calcFrame.add(panel, BorderLayout.LINE_START);
+        calcFrame.add(panel, BorderLayout.PAGE_START);
         
         panel1 = new JPanel();
         
@@ -125,18 +143,108 @@ public class Calculator2
             }
         }
     );
+	    
+	/**dropDown.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                String currentQuantity = (String)dropDown.getSelectedItem();
+		
+		switch(currentQuantity)
+		{
+			case "Boyle's Law":
+				
+		}
+            }
+        });
+    */
    
     }
-    /**
-     * helper2. 
-     * 
-     */
+ 
+    
     public void helper2()
     {
-        JPanel panel2 = new JPanel();
+	panel2.removeAll(); 
+	panel2.updateUI();
+	    
+	var1 = new JLabel();
+        var1.setName("Var1");
+        panel2.add(var1); 
+	
+        field1 = new JTextField();
+        field1.setName("Variable1");
+        field1.setColumns(10);
+        panel2.add(field1); 
+	    
+	un1 = new JLabel();
+        un1.setName("Un1");
+        panel2.add(un1); 
+	    
+	    
+	var2 = new JLabel();
+        var2.setName("Var2");
+        panel2.add(var2); 
+	    
+	field2 = new JTextField();
+        field2.setName("Variable2");
+        field2.setColumns(10);
+        panel2.add(field2); 
+	    
+	un2 = new JLabel();
+        un2.setName("Un2");
+        panel2.add(un2); 
+	    
+	
+	var3 = new JLabel();
+        var3.setName("Var3");
+        panel2.add(var3);     
+	    
+	field3 = new JTextField();
+        field3.setName("Variable3");
+        field3.setColumns(10);
+        panel2.add(field3); 
+	    
+	un3 = new JLabel();
+        un3.setName("Un3");
+        panel2.add(un3);     
+	    
+	var4 = new JLabel();
+        var4.setName("Var4");
+        panel2.add(var4);    
+	    
+	field4 = new JTextField();
+        field4.setName("Variable4");
+        field4.setColumns(10);
+        panel2.add(field4); 
+	    
+	un4 = new JLabel();
+        un4.setName("Un4");
+        panel2.add(un4);     
+        
+        calcFrame.add(panel2, BorderLayout.CENTER);
+    }
+	
+	
+    public void helper3Var()
+    {
+	panel2.removeAll(); 
+	panel2.updateUI();
         
         field = new JTextField();
-        field.setName("infixExpression");
+        field.setName("Equation");
+        field.setColumns(10);
+        panel2.add(field); 
+        
+        calcFrame.add(panel2, BorderLayout.PAGE_START);
+    }
+	
+    public void helper4Var()
+    {
+        panel2.removeAll(); 
+	panel2.updateUI();
+	    
+        field = new JTextField();
+        field.setName("Equation");
         field.setColumns(10);
         panel2.add(field); 
         
